d0108870acbbb1dbe35e1611524ef6a80663b656
Dušan Kováčik <dusan.kovacik@indykite.com>
2022-03-14 12:08:00 +0100

    feat: support for loading configs from credentials

--- packages/web-ts/src/index.tsx
+++ packages/web-ts/src/index.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import InitApp from "./InitApp";
 import { BrowserRouter as Router } from "react-router-dom";
-import { IKUIInit } from "@indykiteone/jarvis-sdk-web";
 
-IKUIInit({
-  baseUri: process.env.REACT_APP_BASE_URI,
-  applicationId: process.env.REACT_APP_APPLICATION_ID,
-  tenantId: process.env.REACT_APP_TENANT_ID,
-});
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <InitApp />
     </Router>
   </React.StrictMode>,
   document.getElementById("root"),
---
+++ packages/web/src/InitApp.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { IKUIInit } from "@indykiteone/jarvis-sdk-web";
+import App from "./App";
+
+let baseUri = process.env.REACT_APP_BASE_URI;
+let applicationId = process.env.REACT_APP_APPLICATION_ID;
+let tenantId = process.env.REACT_APP_TENANT_ID;
+
+function InitApp() {
+  const [initialized, setInitialized] = useState(false);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(process.env.REACT_APP_INDYKITE_APPLICATION_CREDENTIALS_FILE);
+      const credentialsFile = await res.json();
+
+      if (credentialsFile.baseUrl) {
+        baseUri = credentialsFile.baseUrl;
+      }
+      if (credentialsFile.applicationId) {
+        applicationId = credentialsFile.applicationId;
+      }
+      if (credentialsFile.defaultTenantId) {
+        tenantId = credentialsFile.defaultTenantId;
+      }
+
+      IKUIInit({
+        baseUri,
+        applicationId,
+        tenantId,
+      });
+
+      setInitialized(true);
+    };
+
+    if (process.env.REACT_APP_INDYKITE_APPLICATION_CREDENTIALS_FILE) {
+      getData().catch(console.error);
+    } else {
+      IKUIInit({
+        baseUri,
+        applicationId,
+        tenantId,
+      });
+
+      setInitialized(true);
+    }
+  }, []);
+
+  if (!initialized) {
+    return null;
+  }
+
+  return (
+    <App />
+  );
+}
+
+export default InitApp;
