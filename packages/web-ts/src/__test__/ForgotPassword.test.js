13841e2ed51c607f080d5dab59be80b55942c166
andskaal <andreas.skaalerud@indykite.com>
2022-03-30 12:39:19 +0200

    fix: added 16% test coverage

---
+++ packages/web-ts/src/__test__/ForgotPassword.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "../../test-utils";
+import { IKUICore } from "@indykiteone/jarvis-sdk-web";
+import ForgotPassword from "../components/ForgotPassword";
+
+jest.mock("@indykiteone/jarvis-sdk-web", () => {
+  const original = jest.requireActual("@indykiteone/jarvis-sdk-web");
+  return {
+    ...original,
+    IKUICore: {
+      renderForgotPasswordForm: jest.fn().mockImplementation(() => Promise.resolve()),
+    },
+  };
+});
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("All tests for ForgotPassword", () => {
+  test("Test for general render", () => {
+    const { container } = render(<ForgotPassword />);
+    expect(container.querySelector("#forgotten-password-container")).toBeInTheDocument();
+    expect(IKUICore.renderForgotPasswordForm).toBeCalled();
+  });
+});
